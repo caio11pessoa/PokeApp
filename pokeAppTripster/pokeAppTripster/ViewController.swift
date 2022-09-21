6ad67941678606ea823d6e9cfbdf69b203f23b16
user <user@NNT-SERVER-08.local>
2022-09-21 19:35:04 -0300

    Start project

---
+++ pokeAppTripster/pokeAppTripster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokeAppTripster
+//
+//  Created by user on 21/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
